4f47196ef6c1f18179b0fec7a6ad80cd988db5b9
Eric Hendrix <agentmrbig@gmail.com>
2019-12-15 20:51:03 -0500

    initial commit, this small project will be using only react hooks, functional components, and react context api. Other tech to be used will be graphql, styled-components, and more

---
+++ src/components/LoginForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoginForm = props => {
+  return (
+    <form>
+      <input name="username" placeholder="username" />
+      <input name="password" placeholder="password" />
+    </form>
+  );
+};
+
+export default LoginForm;
